e74ae69ab037423b5308052c9e11e1797b70403f
seud abdulsemed <seudsahm@gmail.com>
2026-02-10 18:37:52 +0300

    Force HTTPS schema in production to fix asset loading

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if($this->app->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         \Illuminate\Support\Facades\Gate::policy(\App\Models\User::class, \App\Policies\ProfilePolicy::class);
         
         // Temporarily disable Telescope to reduce log noise
